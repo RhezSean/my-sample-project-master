57cd1a1f973ddc5c4c2fbedac4bb10e04246be20
RhezSean <rhezseancrisostomo@gmail.com>
2017-03-23 14:21:23 +0800

    Add files via upload

---
+++ app/Widget.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+
+class Widget extends SuperModel
+{
+
+    protected $fillable = ['name',
+                           'slug',
+                           'user_id'];
+
+    public function user()
+    {
+
+        return $this->belongsTo('App\User');
+    }
+}
