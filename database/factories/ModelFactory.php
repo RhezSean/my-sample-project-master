ca51f7ab99943b20fe4c9960b75f642a9d807941
RhezSean <rhezseancrisostomo@gmail.com>
2017-03-23 14:40:34 +0800

    Add files via upload

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->safeEmail,
+        'password' => bcrypt(str_random(10)),
+        'remember_token' => str_random(10),
+    ];
+});
+
+
+$factory->define(App\Widget::class, function ($faker) {
+
+    $name = $faker->unique()->word . ' ' . $faker->unique()->word;
+    $slug = str_slug($name, "-");
+    $user_id = rand(1,4);
+
+    return [
+        'name' => $name,
+        'slug' => $slug,
+        'user_id' => $user_id,
+
+
+    ];
+});
+
+$factory->define(App\Gadget::class, function ($faker) {
+
+    $name = $faker->unique()->word . ' ' . $faker->unique()->word;
+
+
+    return [
+        'name' => $name,
+
+
+    ];
+});
